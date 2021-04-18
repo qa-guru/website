7c9125f7b7b13c6924e877ac79ecbe3a01eb822c
Dmitry Zakharov <pumano@gmail.com>
2021-04-18 19:05:29 +0300

    feat(mobile): add more mobile ui

--- components/Register.vue
+++ components/Register.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="flex">
-    <div class="flex-1 flex flex-col">
-      <h3 class="font-bold text-2xl md:text-3xl">Запись на курс</h3>
-      <form class="mt-10 font-3xl placeholder-black flex flex-col space-y-12 md:w-2/3">
-      <input class="focus:outline-none border-b border-solid border-black" placeholder="Имя" />
-      <input class="focus:outline-none border-b border-solid border-black" placeholder="Email" />
-      <input class="focus:outline-none border-b border-solid border-black" placeholder="Телефон" />
-      <qa-button type="primary" class="text-center font-bold w-full">Записаться</qa-button>
+    <div class="md:flex-1 flex flex-col">
+      <h3 class="font-bold text-3xl md:text-center">Запись на курс</h3>
+      <form class="mt-10 text-3xl placeholder-black flex flex-col space-y-12 md:space-y-6 md:w-2/3">
+        <input type="text" class="py-2 focus:outline-none border-b border-solid border-black" placeholder="Имя" />
+        <input type="email" class="py-2 focus:outline-none border-b border-solid border-black" placeholder="Email" />
+        <input type="text" class="py-2 focus:outline-none border-b border-solid border-black" placeholder="Телефон" />
+        <qa-button type="primary" class="text-center font-bold w-full">Записаться</qa-button>
       </form>
       <p class="my-6 md:w-2/3">Отправляя заявку, вы принимаете условия договора оферты и даёте согласие на обработку своих персональных данных в соответствии с политикой конфиденциальности.</p>
     </div>
-    <div class="hidden md:flex-1 md:flex flex-col">
+    <div class="hidden md:flex-1 md:flex flex-col items-center">
+      <div>
       <h3 class="font-bold text-2xl md:text-3xl">Процесс обучения:</h3>
 
       <h5 class="text-xl mt-8">Формат занятий</h5>
@@ -24,6 +25,7 @@
 
       <h5 class="text-xl mt-8">Сохраняем записи всех потоков</h5>
       <p class="font-bold text-3xl">Бессрочный доступ</p>
+      </div>
     </div>
   </div>
 </template>
--- components/Tariffs.vue
+++ components/Tariffs.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="bg-black py-10 text-white">
-    <div class="px-32 flex flex-col justify-center items-center" style="width: 1440px;">
-    <h3 class="font-bold text-white text-6xl mb-12">Выбери тариф для себя</h3>
-    <div class="flex divide-x divide-white">
-      <div class="flex flex-col px-16">
+    <div class="px-6 md:px-32 flex flex-col justify-center items-center">
+    <h3 class="font-bold text-white text-2xl md:text-6xl mb-12">Выбери тариф для себя</h3>
+    <div class="flex flex-col md:flex-row items-center md:items-start space-y-12 md:space-y-0 divide-y md:divide-y-0 md:divide-x divide-white">
+      <div class="flex flex-col md:px-16">
+        <div class="text-2xl">Стоимость обучения</div>
+        <div class="text-6xl mb-2 md:mb-10 font-bold">15 000 ₽</div>
+        <div class="text-3xl mb-2 md:mb-8">Что входит:</div>
+
+        <div class="text-lg">Начало занятий:</div>
+        <div class="text-3xl">29 января</div>
+        <div class="text-sm mb-8">по Московскому времени</div>
+
+         <div class="text-lg">Занятия проходят:</div>
+        <div class="text-3xl">2 раза в неделю</div>
+        <div class="text-sm">по будням в 20:00 по Москве</div>
+      </div>
+      <div class="flex flex-col md:px-16 pt-8 md:pt-0">
         <div class="text-2xl">Стоимость обучения</div>
         <div class="text-6xl mb-10 font-bold">15 000 ₽</div>
         <div class="text-3xl mb-8">Что входит:</div>
@@ -16,20 +29,7 @@
         <div class="text-3xl">2 раза в неделю</div>
         <div class="text-sm">по будням в 20:00 по Москве</div>
       </div>
-      <div class="flex flex-col px-16">
-        <div class="text-2xl">Стоимость обучения</div>
-        <div class="text-6xl mb-10 font-bold">15 000 ₽</div>
-        <div class="text-3xl mb-8">Что входит:</div>
-
-        <div class="text-lg">Начало занятий:</div>
-        <div class="text-3xl">29 января</div>
-        <div class="text-sm mb-8">по Московскому времени</div>
-
-         <div class="text-lg">Занятия проходят:</div>
-        <div class="text-3xl">2 раза в неделю</div>
-        <div class="text-sm">по будням в 20:00 по Москве</div>
-      </div>
-      <div class="flex flex-col px-16">
+      <div class="flex flex-col md:px-16 pt-8 md:pt-0">
         <div class="text-2xl">Стоимость обучения</div>
         <div class="text-6xl mb-10 font-bold">15 000 ₽</div>
         <div class="text-3xl mb-8">Что входит:</div>
